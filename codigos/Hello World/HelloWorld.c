69a01d7021fdad38d005becd602ea2b3b00fb54e
JohnVithor <jv.venceslau.c@gmail.com>
2020-08-21 17:35:50 -0300

    Arquivos adicionados

---
+++ codigos/Hello World/HelloWorld.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <mpi.h>
+
+int main(void) {
+    int my_rank, comm_sz;
+
+    MPI_Init(NULL, NULL);
+    MPI_Comm_size( MPI_COMM_WORLD , &comm_sz);
+    MPI_Comm_rank( MPI_COMM_WORLD , &my_rank);
+
+    printf("Proc %d of %d > Hello World!\n", my_rank+1, com_sz);
+    MPI_Finalize();
+    return 0;
+} /* main */
