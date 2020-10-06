3d7f00af8b97f6a6e2c166e38113933af452f198
JohnVithor <jv.venceslau.c@gmail.com>
2020-10-05 21:30:09 -0300

    Atualizando teste.sh

--- codigos/Trapezios/teste.sh
+++ codigos/Trapezios/teste.sh
@@ -1,47 +1,47 @@
 #!/bin/sh
 
-python3 comparator.py args_teste.json 3 2 0
+python3 comparator.py args_teste.json 12 2 0
 
 mv serial_results.json serial_results1.json
 mv parallel_results.json parallel_results1.json
 
 python3 metrics.py args_teste.json serial_results1.json parallel_results1.json
 
-mv metrics_a.json metrics_a1.json
-mv metrics_b.json metrics_b1.json
+mv metrics_a.json metrics_1a.json
+mv metrics_b.json metrics_1b.json
 
-python3 graphics.py args_teste.json metrics_a1.json
+python3 graphics.py args_teste.json metrics_1a.json
 
-mv eficiencias_cores.png eficiencias_cores_a1.png
-mv eficiencias_problem_size.png eficiencias_problem_size_a1.png
-mv speedups.png speedups_a1.png
+mv eficiencias_cores.png eficiencias_cores_1a.png
+mv eficiencias_problem_size.png eficiencias_problem_size_1a.png
+mv speedups.png speedups_1a.png
 
-python3 graphics.py args_teste.json metrics_b1.json
+python3 graphics.py args_teste.json metrics_1b.json
 
-mv eficiencias_cores.png eficiencias_cores_b1.png
-mv eficiencias_problem_size.png eficiencias_problem_size_b1.png
-mv speedups.png speedups_b1.png
+mv eficiencias_cores.png eficiencias_cores_1b.png
+mv eficiencias_problem_size.png eficiencias_problem_size_1b.png
+mv speedups.png speedups_1b.png
 
 
 
-python3 comparator.py args_teste2.json 3 2 1
+python3 comparator.py args_teste2.json 12 2 1
 
 mv serial_results.json serial_results2.json
 mv parallel_results.json parallel_results2.json
 
-python3 metrics.py args_teste.json serial_results2.json parallel_results2.json
+python3 metrics.py args_teste2.json serial_results2.json parallel_results2.json
 
-mv metrics_a.json metrics_a2.json
-mv metrics_b.json metrics_b2.json
+mv metrics_a.json metrics_2a.json
+mv metrics_b.json metrics_2b.json
 
-python3 graphics.py args_teste.json metrics_a2.json
+python3 graphics.py args_teste2.json metrics_2a.json
 
-mv eficiencias_cores.png eficiencias_cores_a2.png
-mv eficiencias_problem_size.png eficiencias_problem_size_a2.png
-mv speedups.png speedups_a2.png
+mv eficiencias_cores.png eficiencias_cores_2a.png
+mv eficiencias_problem_size.png eficiencias_problem_size_2a.png
+mv speedups.png speedups_2a.png
 
-python3 graphics.py args_teste.json metrics_b2.json
+python3 graphics.py args_teste2.json metrics_2b.json
 
-mv eficiencias_cores.png eficiencias_cores_b2.png
-mv eficiencias_problem_size.png eficiencias_problem_size_b2.png
-mv speedups.png speedups_b2.png
+mv eficiencias_cores.png eficiencias_cores_2b.png
+mv eficiencias_problem_size.png eficiencias_problem_size_2b.png
+mv speedups.png speedups_2b.png
