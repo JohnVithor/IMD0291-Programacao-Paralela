59180c43f9244ad34433722cc41f8abb730dfe70
JohnVithor <jv.venceslau.c@gmail.com>
2020-12-15 21:02:43 -0300

    Atualizando repo com ultimos arquivos

---
+++ Trabalho 5 - OpenMP/src/for_parallel_transpose.c
@@ -0,0 +1,145 @@
+#include <stdio.h>
+#include <errno.h>  // for errno
+#include <math.h>
+#include <limits.h> // for INT_MAX
+#include <stdlib.h> // for strtol
+#include <time.h>
+#include <omp.h>
+
+int numThreads = 1;
+
+void freeMatrix(double** matrix, long lins){
+    for (long i = 0; i < lins; ++i) {
+        free(matrix[i]);
+    }
+    free(matrix);
+}
+
+double** allocMatrix(long lins, long cols){
+    double** matrix = malloc(lins*sizeof(double*));
+    for (long i = 0; i < lins; ++i) {
+        matrix[i] = malloc(cols*sizeof(double));
+    }
+    return matrix;
+}
+
+void printMatrix(double** matrix, long lins, long cols){
+    for (long i = 0; i < lins; ++i) {
+        for (long j = 0; j < cols; ++j)
+            printf("%lf ", matrix[i][j]);
+        printf("\n");
+    }
+    printf("\n");
+}
+
+void fillMatrix(double** matrix, long lins, long cols, long seed){
+    srand(seed);
+    for (long i = 0; i < lins; ++i) {
+        for (long j = 0; j < cols; ++j) {
+            matrix[i][j]= (double) rand() / INT_MAX;
+        }
+    }
+}
+
+void transpose_matrix(double** matrix, double** transposed, long lins, long cols){
+    #pragma omp parallel num_threads(numThreads) default(none) \
+    shared(matrix, transposed, lins, cols)
+    {
+        #pragma omp for schedule(guided)
+        for (long i = 0; i < lins; ++i) {
+            for (long j = 0; j < cols; ++j) {
+                transposed[j][i] = matrix[i][j];
+            }
+        }
+    }
+}
+
+void multiply_row(double* linA, double** B, double* result, long colsB, long size){
+    for (long j = 0; j < colsB; ++j) {
+        result[j] = 0;
+        for (long k = 0; k < size; ++k){
+            result[j] += linA[k] * B[j][k];
+        }
+    }
+}
+
+void multiply_matrix(double** A, double** B, double** result, long linsA, long colsB, long size){
+    #pragma omp parallel num_threads(numThreads) default(none) \
+    shared(linsA, numThreads, A, B, result, colsB, size)
+    {
+        #pragma omp for schedule(guided)
+        for (long i = 0; i < linsA; ++i) {
+            multiply_row(A[i], B, result[i], colsB, size);
+        }
+    }
+}
+
+
+long convert_str_long(char *str){
+    char *p;
+    errno = 0;
+    long conv = strtol(str, &p, 10);
+
+    if (errno != 0 || *p != '\0')
+    {
+        printf("%s não é um número!\n", str);
+        exit(-1);
+    }
+    return (long)conv;
+}
+
+int main(int argc, char **argv){
+
+    if (argc != 9) {
+        printf("É necessário informar os seguintes argumentos:\nO número de threads a serem usadas\nSe as matrizes devem ser exibidas\nSeed para gerar a matriz A\nSeed para gerar a matriz B\nNúmero de linhas de A\nNúmero de colunas de A\nNúmero de linhas de B\nNúmero de colunas de B\n");
+        return -1;
+    }
+
+    numThreads = convert_str_long(argv[1]);
+
+    int show_matrix = convert_str_long(argv[2]);
+
+    long seedA = convert_str_long(argv[3]);
+    long seedB = convert_str_long(argv[4]);
+
+    long linsA = convert_str_long(argv[5]);
+    long colsA = convert_str_long(argv[6]);
+
+    long linsB = convert_str_long(argv[7]);
+    long colsB = convert_str_long(argv[8]);
+
+    if(colsA != linsB){
+        printf("Número de colunas de A é diferente do número de linhas de B, multiplicação não é possivel.\n");
+        return -1;
+    }
+
+    double t = omp_get_wtime();
+
+    double** A = allocMatrix(linsA, colsA);
+    double** B = allocMatrix(linsB, colsB);
+    double** BT = allocMatrix(colsB, linsB);
+    double** R = allocMatrix(linsA, colsB);
+
+    fillMatrix(A, linsA, colsA, seedA);
+    fillMatrix(B, linsB, colsB, seedB);
+
+    transpose_matrix(B, BT, linsB, colsB);
+    multiply_matrix(A, BT, R, linsA, colsB, colsA);
+
+    t = omp_get_wtime() - t; 
+
+    printf("%.10lf\n", t);
+
+    if(show_matrix == 1){
+        printMatrix(A, linsA, colsA);
+        printMatrix(B, linsB, colsB);
+        printMatrix(R, linsA, colsB);
+    }
+    
+    freeMatrix(A, linsA);
+    freeMatrix(B, linsB);
+    freeMatrix(BT, colsB);
+    freeMatrix(R, linsA);
+
+    return 0;
+} /* main */
---
+++ Trabalho 6 - OpenMP/src/serial.c
@@ -0,0 +1,128 @@
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>  // for errno
+#include <math.h>
+#include <limits.h> // for INT_MAX
+#include <stdlib.h> // for strtol
+#include <time.h>
+#include <omp.h>
+
+typedef unsigned int ValueType;
+typedef unsigned int* UnsignedVector;
+
+void print_and_validade_order(UnsignedVector vector, long size){
+    printf("%u", vector[0]);
+    int less = vector[0];
+    for (int i = 1; i < size; ++i) {
+        printf(", %u", vector[i]);
+        if(less > vector[i]){
+            exit(-1);
+        }
+        less = vector[i];
+    }
+}
+
+void validade_order(UnsignedVector vector, long size){
+    int less = vector[0];
+    for (int i = 1; i < size; ++i) {
+        if(less > vector[i]){
+            printf("Not sorted!\n");
+            exit(-1);
+        }
+        less = vector[i];
+    }
+}
+
+void merge_vector(UnsignedVector vector, ValueType left, ValueType mid, ValueType right) {
+    ValueType size_A = right - left + 1;
+    ValueType size_L = mid - left + 1;
+    ValueType size_R = size_A - size_L;
+
+    ValueType* L_aux = malloc(size_L*sizeof(ValueType));
+    ValueType* R_aux = malloc(size_R*sizeof(ValueType));
+
+    memcpy(L_aux, vector + left		, sizeof(ValueType)*size_L);
+    memcpy(R_aux, vector + (mid + 1), sizeof(ValueType)*size_R);
+
+    ValueType i = 0;
+    ValueType j = 0;
+    ValueType k = left;
+
+    while(i < size_L && j < size_R) {
+        vector[k++] = (L_aux[i] < R_aux[j]) ? L_aux[i++] : R_aux[j++];
+    }
+
+    if(i < size_L) {
+        memcpy(vector + k, L_aux + i, sizeof(ValueType)*(size_L-i));
+    }
+    else {
+        memcpy(vector + k, R_aux + j, sizeof(ValueType)*(size_R-j));
+    }
+    
+    free(L_aux);
+    free(R_aux);
+}
+
+void merge_sort_vector_internal (UnsignedVector vector, ValueType left, ValueType right) {
+    if(right > left) {
+        ValueType mid = left + (right - left) / 2;
+        merge_sort_vector_internal(vector, left, mid);
+        merge_sort_vector_internal(vector, mid + 1, right);
+        merge_vector(vector, left, mid, right);
+    }
+}
+
+void merge_sort_vector(UnsignedVector vector, ValueType left, ValueType right) {
+    --right;
+    merge_sort_vector_internal(vector, left, right);
+}
+
+long convert_str_long(char *str){
+    char *p;
+    errno = 0;
+    long conv = strtol(str, &p, 10);
+
+    if (errno != 0 || *p != '\0')
+    {
+        printf("%s não é um número!\n", str);
+        exit(-1);
+    }
+    return (long)conv;
+}
+
+int main( int argc, char **argv ) {
+
+    if (argc != 4) {
+        printf("É necessário informar 3 argumentos na seguinte ordem:\n");
+        printf("1º Seed para gerar os números pseudo-aleatórios\n2º Tamanho do vetor\n3º 1 se o resultado da ordenação deve ser exibido e 0 caso contrário\n");
+        return -1;
+    }
+
+    unsigned int seed = convert_str_long(argv[1]);
+    unsigned int size = convert_str_long(argv[2]);
+    int print_result = convert_str_long(argv[3]);
+
+    srand(seed);
+    UnsignedVector vector = malloc(size*sizeof(ValueType));
+    
+    for (int i = 0; i < size; ++i) {
+        vector[i] = rand();
+    }
+    
+    clock_t t = clock();
+    merge_sort_vector(vector, 0, size);
+    t = clock() - t; 
+    
+    if (print_result == 1){
+        printf("{\"Vetor\": [");
+        print_and_validade_order(vector, size);
+        printf("], \"time\": %.10lf}\n", ((double)t) / CLOCKS_PER_SEC);
+    } else {
+        validade_order(vector, size);
+        printf("%.10lf\n", ((double)t) / CLOCKS_PER_SEC);
+    }
+
+    free(vector);
+
+    return 0;
+}  /* main */
